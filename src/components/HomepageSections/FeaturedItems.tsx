c12017bab237a5153a2e538e55a05c095bac053d
Strozier <strozierrashida@vineyardcolumbus.org>
2024-06-18 14:10:26 -0400

    added responsive tag sizing for boxes

--- src/components/HomepageSections/FeaturedItems.tsx
+++ src/components/HomepageSections/FeaturedItems.tsx
@@ -10,13 +10,6 @@ const FeaturedItems = () => {
                 <div className="featured-title">Featured Item Name 1</div>
                 </div>
             </div>
-            <div className="featured-bucket-item">
-            <div className="featured-items-container">
-                <img alt="image02"></img>
-                <div className="vibe-tag">TODO:VIBE TAG (ex.painting)</div>
-                <div className="featured-title">Featured Item Name 2</div>
-                </div>
-            </div>
         </div>
     </>
   );
