7afb6c8a3b9a40bf5f42ff9e0555bde1b80178d0
Rashida <rashida.strozier@gmail.com>
2024-06-17 20:25:01 -0400

    added test page and both images to the test page

--- src/pages/TestPage.tsx
+++ src/pages/TestPage.tsx
@@ -1,10 +1,21 @@
+import robin1 from '../assets/imgs/robins-art-shack-logo-2.png';
+import robin2 from '../assets/imgs/robins-art-shack-logo.png';
 
 const TestPage = () => {
   return (
     <>
-        <div>Test Page Where You Can Decide!</div>
+      <div className="test-page header-text">
+        Test Page Where You Can Decide Stuff!
+      </div>
+      <div className="image-test-page">
+        <span>Which one do you like better?</span>
+        <div className="image-grid-test">
+          <img src={robin1} alt="Robin's Art Shack Logo 1" />
+          <img src={robin2} alt="Robin's Art Shack Logo 2" />
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default TestPage
+export default TestPage;
