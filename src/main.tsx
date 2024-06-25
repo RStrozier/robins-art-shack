090edd62800c5653735ee5be2d4561b2b5087149
Strozier <strozierrashida@vineyardcolumbus.org>
2024-06-24 23:43:41 -0400

    tanstack and other additions

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App.tsx'
 import './index.css'
+import './App.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './router/routes.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+// create new QueryClient
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} /> 
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} /> 
+      </QueryClientProvider>
   </React.StrictMode>,
 )
